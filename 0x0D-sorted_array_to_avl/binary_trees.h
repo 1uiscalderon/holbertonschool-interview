24470219ec0bf9420cf6c57a543b774b3e465d5b
1uiscalderon <luismiguel.calderone@gmail.com>
2022-03-25 09:04:06 -0500

    changes

--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -1,9 +1,8 @@
-#ifndef BINARY_TREE_H
-#define BINARY_TREE_H
+#ifndef _AVL_H_
+#define _AVL_H_
 
-#include <stddef.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -21,16 +20,10 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 };
 
-typedef struct binary_tree_s binary_tree_t;
-/* AVL Tree */
 typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s binary_tree_t;
 
-/* Create new node */
-binary_tree_t *NewNode(binary_tree_t *parent, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
-avl_t *array_to_avl(int *array, int start, int size, avl_t *);
+void binary_tree_print(const binary_tree_t *tree);
 
-/* Function to print Binary Tree */
-void binary_tree_print(const binary_tree_t *);
-
-#endif /* Convert array sorted to AVL Binary tree */
+#endif
